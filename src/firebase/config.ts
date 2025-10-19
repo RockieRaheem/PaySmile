4eb1bddd69195b46054e51bc9511b74cd05460aa
Kamwanga Rahiim <kamwangaraheem2050@gmail.com>
2025-10-19 19:35:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8630735098-5df31",
+  "appId": "1:744302660721:web:bc686813ca5a2ddccc1fd0",
+  "apiKey": "AIzaSyBkg4_6XFRPY5YKNKNLXZ7-yk5HDxB18OI",
+  "authDomain": "studio-8630735098-5df31.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "744302660721"
+};
